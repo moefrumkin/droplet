ba6a99b91ed37776be36ef47607c940eafe127df
Moe Frumkin <moegf1@gmail.com>
2021-03-25 16:12:19 -0400

    Fix Javadoc Errors preventing release

--- src/main/java/com/moefrumkin/droplet/parser/statement/package-info.java
+++ src/main/java/com/moefrumkin/droplet/parser/statement/package-info.java
@@ -1,14 +1,4 @@
 /**
  * A package that contains the classes to represent and parse statements.
- * In droplet a statement can be represented by the following context free grammar:
- * <ul>
- * 		<li> Statement -> e | CompoundStatement | ConditionalStatement | DeclarationStatement | ExpressionStatement | LoopStatement | ReturnStatement </li>
- *  	<li> CompoundStatement -> { List[Statement] } </li>
- *		<li> ConditionalStatement -> if (Expression) Statement </li>
- *      <li> DeclarationStatement -> Identifier = Expression </li>
- *      <li> ExpressionStatement -> Expression; </li>
- *      <li> LoopStatement -> while(Expression) Statement </li>
- *		<li> ReturnStatement -> return Expression; </li>
- * </ul>
  */
 package com.moefrumkin.droplet.parser.statement;
